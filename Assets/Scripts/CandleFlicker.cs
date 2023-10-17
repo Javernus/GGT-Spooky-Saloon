01315e3839968c3710d6e00060b8ad90212f617c
Jake Jongejans <jake.jongejans@enreach.com>
2023-10-17 12:21:02 +0200

    feat: Spooky candles.

--- Assets/Scripts/CandleFlicker.cs
+++ Assets/Scripts/CandleFlicker.cs
@@ -2,30 +2,71 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CandleMovement : MonoBehaviour
+public class CandleFlicker : MonoBehaviour
 {
-    public float speed = 1.0f;
-    public float amplitude = 0.5f;
+    private Color[] beforeColours;
+    private Color[] afterColours;
 
-    // Define private colour
-    private Color colour;
+    private float[] differences;
+    private float[] durations;
+
+    Color GenerateNewColour() {
+        float hue = Random.Range(0.02f, 0.11f);
+        float saturation = Random.Range(0.5f, 1.0f);
+        float value = Random.Range(0.875f, 1.0f);
+
+        return Color.HSVToRGB(hue, saturation, value);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        colour = GetComponent<Renderer>().material.color;
+        // Fill beforeColours with current colours of child candles
+        beforeColours = new Color[transform.childCount];
+
+        // Create a random candlish colour for each candle as the afterColour
+        afterColours = new Color[transform.childCount];
+
+        // Create a random duration for each candle and set difference to 0
+        differences = new float[transform.childCount];
+        durations = new float[transform.childCount];
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            beforeColours[i] = transform.GetChild(i).GetChild(2).GetComponent<Light>().color;
+
+            afterColours[i] = GenerateNewColour();
+
+            differences[i] = 0.0f;
+            durations[i] = Random.Range(0.0125f, 0.5f);
+
+        }
+    }
+
+    void UpdateCandle(Transform candle, Color beforeColour, Color afterColour, float difference, float duration, int i) {
+        float mixRatio = Mathf.Clamp(Time.time - difference, 0.0f, Mathf.Infinity) / duration;
+
+        // Change the colour of the candle's first material and the light's colour
+        candle.GetChild(1).GetComponent<MeshRenderer>().materials[0].SetVector("_EmissionColor", Color.Lerp(beforeColour, afterColour, mixRatio));
+        candle.GetChild(2).GetComponent<Light>().color = Color.Lerp(beforeColour, afterColour, mixRatio);
+
+        if (mixRatio >= 1.0f) {
+            Debug.Log("New colour " + i + ", " + Time.time + ", " + difference + ", " + duration + ", " + mixRatio);
+            differences[i] = Time.time;
+            durations[i] = Random.Range(0.0125f, 0.5f);
+
+            beforeColours[i] = afterColour;
+            afterColours[i] = GenerateNewColour();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Move the candle up and down
-        // transform.position = new Vector3(transform.position.x, transform.position.y + Mathf.Sin(Time.time * speed) * amplitude, transform.position.z);
-
-        // Change the colour of the candle
-        colour.r = Mathf.Sin(Time.time * speed) * amplitude;
-        colour.g = Mathf.Sin(Time.time * speed) * amplitude;
-        colour.b = Mathf.Sin(Time.time * speed) * amplitude;
-        GetComponent<Renderer>().material.color = colour;
+        // Update the colours of the candles
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            UpdateCandle(transform.GetChild(i), beforeColours[i], afterColours[i], differences[i], durations[i], i);
+        }
     }
 }
--- Assets/Scripts/CandleMovement.cs
+++ Assets/Scripts/CandleMovement.cs
@@ -2,15 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CandleFlicker : MonoBehaviour
+public class CandleMovement : MonoBehaviour
 {
+    // Create an array for initial y values for all child candles
+    private float[] initialYs;
+
+    // Create random speed, amplitude and delay for each candle
+    private float[] speeds;
+    private float[] amplitudes;
+    private float[] delays;
+    private float[] offsets;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        // Store the initial Y position of the child candles
+        initialYs = new float[transform.childCount];
+
+        // Create random speed, amplitude and delay for each candle
+        speeds = new float[transform.childCount];
+        amplitudes = new float[transform.childCount];
+        delays = new float[transform.childCount];
+        offsets = new float[transform.childCount];
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            initialYs[i] = transform.GetChild(i).position.y;
+            speeds[i] = Random.Range(0.5f, 2.5f);
+            amplitudes[i] = Random.Range(0.125f, 0.25f);
+            delays[i] = Random.Range(0.0f, 5.0f);
+            offsets[i] = Random.Range(0.125f, 0.5f);
+        }
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+        }
+    }
+
+    void MoveCandle(Transform candle, float initialY, float speed, float amplitude, float delay, float offset) {
+        float time = Mathf.Clamp(Time.time - delay, 0.0f, Mathf.Infinity);
+        float extraUp = Mathf.Clamp(Time.time - delay, 0.0f, 1.0f) * offset;
+
+        candle.position = new Vector3(candle.position.x, initialY + (1.0f - Mathf.Cos(time * speed)) * 0.5f * amplitude + extraUp, candle.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Move the candles up and down
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            MoveCandle(transform.GetChild(i), initialYs[i], speeds[i], amplitudes[i], delays[i], offsets[i]);
+        }
     }
 }
