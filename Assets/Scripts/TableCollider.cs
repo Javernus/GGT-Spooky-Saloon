2d651ec175346850b8782d05a13a14d38ad46114
Merijn <merijn.laks@student.uva.nl>
2023-10-18 21:44:56 +0200

    feat: table box collision detection

--- Assets/Scripts/TableCollider.cs
+++ Assets/Scripts/TableCollider.cs
@@ -1,9 +1,57 @@
+using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
 public class TableCollider : MonoBehaviour
 {
-    private void OnCollisionEnter(UnityEngine.Collision collision)
+    // table box collider
+    public BoxCollider tableCollider;
+
+    // list of balls
+    public SphereCollider[] ballColliders;
+
+    // Start is called before the first frame update
+    void Start()
     {
-        Debug.Log("Collision detected");
+        // get table game object
+        GameObject poolTabel = GameObject.Find("PoolTafel");
+
+        // get table collider
+        tableCollider = poolTabel.GetComponent<BoxCollider>();
+
+        if (tableCollider == null)
+        {
+            Debug.Log("TableCollider: tableCollider is null");
+        }
+
+        // get ball colliders
+        ballColliders = GetComponentsInChildren<SphereCollider>();
+
+        if (ballColliders == null)
+        {
+            Debug.Log("TableCollider: ballColliders is null");
+        }
+    }
+
+    bool Is_in_table(Vector3 position)
+    {
+        return (
+            position.x > tableCollider.bounds.min.x &&
+            position.x < tableCollider.bounds.max.x &&
+            position.z > tableCollider.bounds.min.z &&
+            position.z < tableCollider.bounds.max.z
+        );
+    }
+
+    void FixedUpdate()
+    {
+        // for every ball, check if it is in table
+        foreach (SphereCollider ballCollider in ballColliders)
+        {
+            if (!Is_in_table(ballCollider.bounds.center))
+            {
+                Debug.Log("Ball is out of table");
+
+            }
+        }
     }
 
 }
